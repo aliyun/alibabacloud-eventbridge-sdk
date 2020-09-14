56924878395c157d8c40f497862225cdcfae6a0b
824750130 <e824750130@qq.com>
2020-09-14 16:03:27 +0800

    fix test case

--- util/python/tests/test_client.py
+++ util/python/tests/test_client.py
@@ -1,13 +1,74 @@
 import unittest
 from Tea.request import TeaRequest
+from Tea.model import TeaModel
 from alibabacloud_eventbridge_util.client import Client
 
 
 class TestClient(unittest.TestCase):
+    class CloudEvent(TeaModel):
+        """
+        Cloud Event Stanard Froamt
+        """
+
+        def __init__(self, id=None, source=None, specversion=None, type=None, datacontenttype=None, dataschema=None,
+                     subject=None, time=None, extensions=None, data=None):
+            self.id = id  # type: str
+            self.source = source  # type: str
+            self.specversion = specversion  # type: str
+            self.type = type  # type: str
+            self.datacontenttype = datacontenttype  # type: str
+            self.dataschema = dataschema  # type: str
+            self.subject = subject  # type: str
+            self.time = time  # type: str
+            self.extensions = extensions  # type: Dict[str, Any]
+            self.data = data  # type: bytes
+
+        def validate(self):
+            self.validate_required(self.id, 'id')
+            self.validate_required(self.source, 'source')
+            if self.source:
+                self.validate_max_length(self.source, 'source', 128)
+            self.validate_required(self.specversion, 'specversion')
+            self.validate_required(self.type, 'type')
+            if self.type:
+                self.validate_max_length(self.type, 'type', 64)
+            if self.time:
+                self.validate_max_length(self.time, 'time', 64)
+                self.validate_pattern(self.time, 'time',
+                                      '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}[\\s\\S]*')
+            self.validate_required(self.extensions, 'extensions')
+
+        def to_map(self):
+            result = {}
+            result['id'] = self.id
+            result['source'] = self.source
+            result['specversion'] = self.specversion
+            result['type'] = self.type
+            result['datacontenttype'] = self.datacontenttype
+            result['dataschema'] = self.dataschema
+            result['subject'] = self.subject
+            result['time'] = self.time
+            result['extensions'] = self.extensions
+            result['data'] = self.data
+            return result
+
+        def from_map(self, map={}):
+            self.id = map.get('id')
+            self.source = map.get('source')
+            self.specversion = map.get('specversion')
+            self.type = map.get('type')
+            self.datacontenttype = map.get('datacontenttype')
+            self.dataschema = map.get('dataschema')
+            self.subject = map.get('subject')
+            self.time = map.get('time')
+            self.extensions = map.get('extensions')
+            self.data = map.get('data')
+            return self
+
     def test_get_string_to_sign(self):
         request = TeaRequest()
         str_to_sign = Client.get_string_to_sign(request)
-        self.assertEqual('GET\n\n\n\n\n', str_to_sign)
+        self.assertEqual('GET\n\n\n\n', str_to_sign)
 
         request = TeaRequest()
         request.method = "POST"
@@ -15,14 +76,14 @@ class TestClient(unittest.TestCase):
             'test': 'tests'
         }
         str_to_sign = Client.get_string_to_sign(request)
-        self.assertEqual('POST\n\n\n\n\n?test=tests', str_to_sign)
+        self.assertEqual('POST\n\n\n\n?test=tests', str_to_sign)
 
         request = TeaRequest()
         request.headers = {
             'content-md5': 'md5',
         }
         str_to_sign = Client.get_string_to_sign(request)
-        self.assertEqual('GET\n\nmd5\n\n\n', str_to_sign)
+        self.assertEqual('GET\nmd5\n\n\n', str_to_sign)
 
         request = TeaRequest()
         request.pathname = "Pathname"
@@ -39,34 +100,35 @@ class TestClient(unittest.TestCase):
             'date': 'date'
         }
         str_to_sign = Client.get_string_to_sign(request)
-        s = 'GET\napplication/json\nmd5\napplication/json\ndate\nx-acs-meta:user\nPathname?ccp=ok&test=tests&test1'
+        s = 'GET\nmd5\napplication/json\ndate\nx-acs-meta:user\nPathname?ccp=ok&test=tests&test1='
         self.assertEqual(s, str_to_sign)
 
     def test_get_signature(self):
         request = TeaRequest()
         str_to_sign = Client.get_string_to_sign(request)
         signature = Client.get_signature(str_to_sign, 'secret')
-        self.assertEqual('GET\n\n\n\n\n', str_to_sign)
-        self.assertEqual('XGXDWA78AEvx/wmfxKoVCq/afWw=', signature)
+        self.assertEqual('GET\n\n\n\n', str_to_sign)
+        self.assertEqual('Mzu+31oD5IDdv3WFBNIi4Y7JZto=', signature)
 
     def test_serialize(self):
+        test_model1 = self.CloudEvent(
+            datacontenttype='application/xml',
+            data=b'base64encode',
+            extensions={
+                    'key1': 'value1',
+                    'key2': 'value2',
+                }
+        )
+        test_model2 = self.CloudEvent(
+            datacontenttype='application/xml',
+            data=b'base64encode',
+            extensions={
+                    'key1': 'value1',
+                    'key2': 'value2',
+                }
+        )
         events = [
-            {
-                'datacontenttype': 'application/xml',
-                'data': 'base64encode',
-                'extensions': {
-                    'key1': 'value1',
-                    'key2': 'value2',
-                }
-            },
-            {
-                'datacontenttype': 'application/xml',
-                'data': 'base64encode',
-                'extensions': {
-                    'key1': 'value1',
-                    'key2': 'value2',
-                }
-            }
+            test_model1, test_model2
         ]
         res = Client.serialize(events)
         tmp = [
